745ebfa8aeedd11afe90428f41c3d3bf6fe2c1f7
Sharan Raj R <hemistryzwing@gmail.com>
2026-09-11 03:07:20 +0530

    Initial commit: Blog Web App structure, styling, and initial server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import ejs from "ejs";
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
